535ee8ba874692fbf6936726aee35b480144bf9e
Johnattan Silva <115963954+JahnoMon@users.noreply.github.com>
2023-10-16 10:42:46 -0300

    Update Q1.java

--- Q1.java
+++ Q1.java
@@ -1,1 +1,12 @@
+/*
+  Uma API (Application Programming Interface) é como uma padronização para a construção e conexão de aplicativos. Funciona de
+  maneira análoga a um restaurante.
 
+  Em um restaurante temos clientes, o garçom e o cozinheiro. Os clientes seriam o browser (ou usuários mesmos), o garçom
+  seria a API e o cozinheiro seria um servidor (banco de dados, por exemplo). Nós precisamos de alimento tal como o browser
+  precisa de dados/informações. Nós (usuários) fazemos um pedido para o garçom (API) que recebe o pedido e escreve na comanda
+  de forma que o cozinheiro entenda, ou seja, a API recebe pedido do usuário e faz algum processamento para o servidor retornar
+  informações. Por fim, com o pedido realizado, o cozinheiro prepara a comida, ou seja, o banco de dados prepara os dados de 
+  acordo com o pedido, e no fim, a API pega as informações (cozinheiro pega a comida) e entrega ao usuário, o qual não tem de 
+  se preocupar com nada mais além de apenas fazer o pedido (o resto fica por conta da API).
+*/
