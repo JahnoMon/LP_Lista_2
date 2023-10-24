2d696f3df645abcec3778f830a78fbd2ca7de588
Johnattan Silva <115963954+JahnoMon@users.noreply.github.com>
2023-10-24 00:35:19 -0300

    Update Q2.java

--- Q2.java
+++ Q2.java
@@ -10,75 +10,78 @@ console.
 */
 
 // interface
-package questoes;
+package exemplos;
 
-public interface Drawable {
-	void draw();
-}
+public class E1 {
 
-// Classe 1:
+	// Classe 1:
 
-public class Quadrado implements Drawable{
-	
-	public Quadrado() {
-	}
-	
-	@Override
-	public void draw() {
-		System.out.println("O O O O O\n"
-				+ "O       O\n"
-				+ "O       O\n"
-				+ "O       O\n"
-				+ "O O O O O");
-	}
-	
-	 public static void main(String[] args) {
-	        
-		 Quadrado quadrado = new Quadrado();
-		 
-		 quadrado.draw();
-	 }
-}
-
-// Classe 2:
-
-public class Arvore implements Drawable {
-	
-	public Arvore() {
+	public static class Quadrado implements Drawable{
 		
-	}
-	
-	@Override
-	public void draw() {
-		System.out.printf("%3s\n%4s\n%s\n%3s", "O", "OOO", "OOOOO","|");
-	}
-	
-	
-	 public static void main(String[] args) {
-	        
-		 Arvore arvore = new Arvore();
-		 
-		 arvore.draw();
-	 }
-}
-
-// classe 3: 
-
-public class Triangulo implements Drawable{
-
-	public Triangulo() {
+		public Quadrado() {
+		}
 		
+		@Override
+		public void draw() {
+			System.out.println(
+					  "O O O O O\n"
+					+ "O       O\n"
+					+ "O       O\n"
+					+ "O       O\n"
+					+ "O O O O O\n");
+		}
+	}
+
+	// Classe 2:
+
+	public static class Arvore implements Drawable {
+		
+		public Arvore() {}
+		
+		@Override
+		public void draw() {
+			System.out.printf("%3s\n%4s\n%s\n%3s\n", "O", "OOO", "OOOOO","|");
+		}
+	}
+
+	// classe 3: 
+
+	public class Triangulo implements Drawable{
+
+		public Triangulo() {}
+		
+		@Override
+		public void draw() {
+			System.out.printf("%s\n%9s\n%9s\n%9s\n%s\n", "    O    ", "   O O   ", "  O   O  "," O     O ","O O O O O");
+		}
 	}
 	
-	@Override
-	public void draw() {
-		System.out.printf("%s\n%9s\n%9s\n%9s\n%s", "O O O O O", "O       O", "O       O","O       O","O O O O O");
+	public static class Desenho implements Drawable {
+		
+		private Drawable desenho;
+		
+		public Desenho(Drawable desenho) {
+			this.desenho = desenho;
+		}
+		
+		@Override
+		public void draw() {
+			desenho.draw();
+		}
+		
+		
 	}
 	
 	public static void main(String[] args) {
 		
-		Triangulo triangulo = new Triangulo();
+		Quadrado quadrado = new Quadrado();
+		quadrado.draw();
 		
+		// sem o E1() -> No enclosing instance of type E1 is accessible. Must qualify the allocation with an enclosing instance of type E1 (e.g. x.new A() where x is an instance of E1)
+		Triangulo triangulo = new E1(). new Triangulo();
 		triangulo.draw();
+
+		Arvore arvore = new Arvore();
+		arvore.draw();		
 	}
 }
